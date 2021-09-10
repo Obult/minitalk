f5cfbf37ebadb4e313e0a12befc2a6fdd0f0045d
bult <oswin.bult@gmail.com>
2021-09-10 11:42:10 -0400

    msg

--- server/server.c
+++ server/server.c
@@ -6,17 +6,20 @@
 #include "utils.h"
 
 t_decoding  msg;
-char unsigned string[] = "\0\0\0\0\0\0\0\0\0\0\0\0\0";
-unsigned char	*ft_increase(unsigned char **str, size_t len);
- 
-void    sig_handler(int signum, siginfo_t *info, void *q)
+
+unsigned char	*ft_increase(unsigned char **str, size_t len)
 {
-	info = 0;
-	q = 0;
-	info++;
-	q++;
-
-
+	unsigned char *holder;
+	if(!len)
+		return(malloc(16 * sizeof(char)));
+	holder = malloc((len + 16) * sizeof(char));
+	holder = ft_memcpy(*str, holder, len);
+	free(*str);
+	return (holder);
+}
+ 
+void    sig_handler(int signum)
+{
     msg.string[msg.i] = (msg.string[msg.i] >> 1);
     if (signum == SIGUSR1)
 	{
@@ -48,19 +51,20 @@ void    sig_handler(int signum, siginfo_t *info, void *q)
  
 int     main(void)
 {
-    struct sigaction   act, act2;
+    struct sigaction   act;
     pid_t   pid;
+
 
 	msg.j = 0;
 	msg.i = 0;
-    // msg.string = string;
 	msg.string = ft_increase(0, 0);
  
-    act.sa_sigaction = sig_handler;
-    act.sa_flags = SA_SIGINFO;
+	act.sa_handler = sig_handler;
+    // act.sa_sigaction = sig_handler;
+    // act.sa_flags = SA_SIGINFO;
  
-    act2.sa_sigaction = sig_handler;
-    act2.sa_flags = SA_SIGINFO;
+    // act2.sa_sigaction = sig_handler;
+    // act2.sa_flags = SA_SIGINFO;
  
     pid = getpid();
 	write(1, "pid = ", 6);
@@ -68,20 +72,8 @@ int     main(void)
 	write(1, "\n", 1);
  
     sigaction(SIGUSR1, &act, 0);
-    sigaction(SIGUSR2, &act2, 0);
+    sigaction(SIGUSR2, &act, 0);
 	while (1)
 		sleep(1);
- 
     return (0);
-}
-
-unsigned char	*ft_increase(unsigned char **str, size_t len)
-{
-	unsigned char *holder;
-	if(!len)
-		return(malloc(16 * sizeof(char)));
-	holder = malloc((len + 16) * sizeof(char));
-	holder = ft_memcpy(*str, holder, len);
-	free(*str);
-	return (holder);
 }
